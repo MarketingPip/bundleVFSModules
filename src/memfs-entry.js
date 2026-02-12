e59070b199f5294efd71d8c15deb2c1de317d4bc
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-12 06:58:54 -0500

    Refactor memfs entry to export Node-like fs object

--- src/memfs-entry.js
+++ src/memfs-entry.js
@@ -1,2 +1,15 @@
-import * as memfs from "memfs";
-export default memfs;
+import { vol, promises, constants, fs as memfsFs } from "memfs";
+import { createFsFromVolume } from "memfs";
+
+// Create a Node-like fs object from the memfs volume
+const fs = createFsFromVolume(vol);
+
+// Attach the promises API (exact Node-style)
+fs.promises = promises;
+
+// Also attach constants (Node-style)
+fs.constants = constants;
+
+// Export the Node-like fs object and named exports
+export default fs;
+export { fs, promises, constants };
