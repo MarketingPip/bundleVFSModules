5be76c357a3fd8de8875aac42eabcd39067fe387
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-12 06:47:01 -0500

    Update build-vfs.mjs

--- src/build-vfs.mjs
+++ src/build-vfs.mjs
@@ -2,7 +2,7 @@ import { build } from "esbuild";
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import polyfillNode from "esbuild-plugin-node-modules-polyfill";
+import polyfillNode from "esbuild-plugins-node-modules-polyfill";
 // Get __dirname equivalent in ESM
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
