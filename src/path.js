7ebf82b67833a5933e05ebded2bce4bbd5e9749a
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-15 13:26:20 -0500

    Update path.js

--- src/path.js
+++ src/path.js
@@ -1,4 +1,4 @@
 import path from "path-browserify";
 
 export * from "path-browserify";  // export all named exports
-export default path;              // keep default export
+// export default path;              // keep default export
