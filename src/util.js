1002a7f9275727336d38e49be87449bda1314318
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-15 11:18:07 -0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -2,11 +2,11 @@ import util from "util"
 'use strict';
 
 // The sys module was renamed to 'util'. This shim remains to keep old programs
-// working. `sys` is deprecated and shouldn't be used.
-
-// Note to maintainers: Although this module has been deprecated for a while
-// we do not plan to remove it. See: https://github.com/nodejs/node/pull/35407#issuecomment-700693439
+ 
+ /*
+TODO implement emitWarning: https://github.com/nodejs/node/blob/9cc7fcc26dece769d9ffa06c453f0171311b01f8/lib/internal/process/warning.js#L138 
  
 process.emitWarning('sys is deprecated. Use `node:util` instead.',
                     'DeprecationWarning', 'DEP0025');
+ */                    
 export default util
