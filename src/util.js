6598f4a653ef027b0eccf92f6ee90084ddf1ce46
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-15 13:24:02 -0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,12 +1,10 @@
-import util from "util"
 'use strict';
 
-// The sys module was renamed to 'util'. This shim remains to keep old programs
- 
- /*
-TODO implement emitWarning: https://github.com/nodejs/node/blob/9cc7fcc26dece769d9ffa06c453f0171311b01f8/lib/internal/process/warning.js#L138 
- 
-process.emitWarning('sys is deprecated. Use `node:util` instead.',
-                    'DeprecationWarning', 'DEP0025');
- */                    
-export default util
+// Import all named exports as a namespace
+import * as util from 'util';
+
+// Export everything as named exports
+export * from 'util';
+
+// Export the namespace as default
+// export default util;
