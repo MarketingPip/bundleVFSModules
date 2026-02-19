cb8c73a0a18e7299a3d84dc0955cd4599e0046b2
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-19 17:41:58 -0500

    Update sys.js

--- src/sys.js
+++ src/sys.js
@@ -4,11 +4,9 @@
 
 // The sys module was renamed to 'util'. This shim remains to keep old programs
  
- /*
-TODO implement emitWarning: https://github.com/nodejs/node/blob/9cc7fcc26dece769d9ffa06c453f0171311b01f8/lib/internal/process/warning.js#L138 
- 
-process.emitWarning('sys is deprecated. Use `node:util` instead.',
+if(globalThis && globalThis?.process){
+process?.emitWarning('sys is deprecated. Use `node:util` instead.',
                     'DeprecationWarning', 'DEP0025');
- */                    
+}
  
 export default util; 
