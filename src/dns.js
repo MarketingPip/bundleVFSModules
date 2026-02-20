d383cbb89bbb5a26ab85a84718d7abb74098b812
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-02-19 20:36:57 -0500

    Update dns.js

--- src/dns.js
+++ src/dns.js
@@ -300,37 +300,37 @@ const defer = fn => setTimeout(fn, 0)
 /**
  * Real-ish Lookup using DoH
  */
-export function lookup(hostname, optionsOrCallback, callback) {
-  const cb =
-    typeof optionsOrCallback === "function" ? optionsOrCallback : callback
-  const options = typeof optionsOrCallback === "object" ? optionsOrCallback : {}
+export function function lookup(hostname, optionsOrCallback, callback) {
+  const cb = typeof optionsOrCallback === "function" ? optionsOrCallback : callback;
+  const options = typeof optionsOrCallback === "object" ? optionsOrCallback : {};
 
   // Handle localhost instantly
   if (hostname === "localhost" || hostname === "127.0.0.1") {
     defer(() => {
-      if (options.all) cb(null, [{ address: "127.0.0.1", family: 4 }])
-      else cb(null, "127.0.0.1", 4)
-    })
-    return
+      if (options.all) cb(null, [{ address: "127.0.0.1", family: 4 }]);
+      else cb(null, "127.0.0.1", 4);
+    });
+    return;
   }
 
-  // Perform DoH Query
   resolver
     .query(hostname, "A")
     .then(response => {
-      const answers = response.filter(a => a.type === "A")
+      // Ensure answer exists and is an array
+      const answers = (response.answer || []).filter(a => a.type === "A");
+
       if (answers.length === 0) {
-        return cb(new Error(`ENOTFOUND ${hostname}`))
+        return cb(new Error(`ENOTFOUND ${hostname}`));
       }
 
       if (options.all) {
-        const results = answers.map(a => ({ address: a.data, family: 4 }))
-        cb(null, results)
+        const results = answers.map(a => ({ address: a.data, family: 4 }));
+        cb(null, results);
       } else {
-        cb(null, answers[0].data, 4)
+        cb(null, answers[0].data, 4);
       }
     })
-    .catch(err => cb(err))
+    .catch(err => cb(err));
 }
 
 /**
@@ -340,22 +340,20 @@ export function resolve4(hostname, callback) {
   resolver
     .query(hostname, "A")
     .then(response => {
-      const ips = response.filter(a => a.type === "A").map(a => a.data)
-      callback(null, ips)
+      const ips = (response.answer || []).filter(a => a.type === "A").map(a => a.data);
+      callback(null, ips);
     })
-    .catch(err => callback(err))
+    .catch(err => callback(err));
 }
 
 export function resolve6(hostname, callback) {
   resolver
     .query(hostname, "AAAA")
     .then(response => {
-      const ips = response.answer
-        .filter(a => a.type === "AAAA")
-        .map(a => a.data)
-      callback(null, ips)
+      const ips = (response.answer || []).filter(a => a.type === "AAAA").map(a => a.data);
+      callback(null, ips);
     })
-    .catch(err => callback(err))
+    .catch(err => callback(err));
 }
 
 // Map general resolve to resolve4 for simplicity
