0d54a2b66983d6dc5cb75e17e138a99b8b0fa59e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-05 07:55:43 -0500

    Update domain.js

--- src/domain.js
+++ src/domain.js
@@ -1,58 +1,201 @@
-/**
- * Node.js domain module shim
- * The domain module is deprecated but some packages still use it
- */
+const EventEmitter = require("events");
 
-import { EventEmitter } from "./events"
+const stack = [];
 
-export class Domain extends EventEmitter {
-  members = []
+function active() {
+  return stack.length ? stack[stack.length - 1] : null;
+}
+
+function updateProcessDomain() {
+  process.domain = active();
+}
+
+class Domain extends EventEmitter {
+  constructor() {
+    super();
+    this.members = [];
+    this._disposed = false;
+  }
 
   add(emitter) {
-    this.members.push(emitter)
+    if (!emitter || this._disposed) return this;
+
+    if (emitter.domain && emitter.domain !== this) {
+      emitter.domain.remove(emitter);
+    }
+
+    emitter.domain = this;
+    this.members.push(emitter);
+
+    const d = this;
+
+    function onError(err) {
+      annotate(err, {
+        domain: d,
+        domainEmitter: emitter,
+        domainThrown: false
+      });
+
+      d.emit("error", err);
+    }
+
+    emitter.on("error", onError);
+    emitter.__domainHandler = onError;
+
+    return this;
   }
 
   remove(emitter) {
-    const index = this.members.indexOf(emitter)
-    if (index !== -1) {
-      this.members.splice(index, 1)
+    const i = this.members.indexOf(emitter);
+    if (i !== -1) this.members.splice(i, 1);
+
+    if (emitter.__domainHandler) {
+      emitter.removeListener("error", emitter.__domainHandler);
+      delete emitter.__domainHandler;
     }
-  }
 
-  bind(callback) {
-    return callback
-  }
+    emitter.domain = null;
 
-  intercept(callback) {
-    return callback
-  }
-
-  run(fn) {
-    return fn()
-  }
-
-  dispose() {
-    this.members = []
+    return this;
   }
 
   enter() {
-    // Stub
+    if (this._disposed) return;
+
+    stack.push(this);
+    Domain.active = this;
+    updateProcessDomain();
   }
 
   exit() {
-    // Stub
+    if (this._disposed) return;
+
+    while (stack.length) {
+      const d = stack.pop();
+      if (d === this) break;
+    }
+
+    Domain.active = active();
+    updateProcessDomain();
+  }
+
+  run(fn, ...args) {
+    if (this._disposed) return;
+
+    this.enter();
+
+    try {
+      return fn.apply(this, args);
+    } catch (err) {
+      annotate(err, {
+        domain: this,
+        domainThrown: true
+      });
+
+      this.emit("error", err);
+    } finally {
+      this.exit();
+    }
+  }
+
+  bind(fn) {
+    const d = this;
+
+    function bound(...args) {
+      if (d._disposed) return;
+
+      d.enter();
+
+      try {
+        return fn.apply(this, args);
+      } catch (err) {
+        annotate(err, {
+          domain: d,
+          domainBound: bound,
+          domainThrown: true
+        });
+
+        d.emit("error", err);
+      } finally {
+        d.exit();
+      }
+    }
+
+    bound.domain = d;
+
+    return bound;
+  }
+
+  intercept(fn) {
+    const d = this;
+
+    function intercepted(err, ...args) {
+      if (d._disposed) return;
+
+      if (err) {
+        annotate(err, {
+          domain: d,
+          domainBound: intercepted,
+          domainThrown: false
+        });
+
+        d.emit("error", err);
+        return;
+      }
+
+      d.enter();
+
+      try {
+        return fn.apply(this, args);
+      } catch (e) {
+        annotate(e, {
+          domain: d,
+          domainBound: intercepted,
+          domainThrown: true
+        });
+
+        d.emit("error", e);
+      } finally {
+        d.exit();
+      }
+    }
+
+    intercepted.domain = d;
+
+    return intercepted;
+  }
+
+  dispose() {
+    if (this._disposed) return;
+
+    this._disposed = true;
+
+    for (const m of this.members.slice()) {
+      this.remove(m);
+    }
+
+    this.members.length = 0;
   }
 }
 
-export function create() {
-  return new Domain()
+function annotate(err, fields) {
+  if (!err || typeof err !== "object") return;
+
+  if (fields.domain) err.domain = fields.domain;
+  if (fields.domainEmitter) err.domainEmitter = fields.domainEmitter;
+  if (fields.domainBound) err.domainBound = fields.domainBound;
+  if (fields.domainThrown !== undefined) err.domainThrown = fields.domainThrown;
 }
 
-// Active domain (deprecated but some packages check for it)
-export let active = null
+Domain.active = null;
 
-export default {
-  Domain,
+function create() {
+  return new Domain();
+}
+
+module.exports = {
   create,
+  createDomain: create,
+  Domain,
   active
-}
+};
