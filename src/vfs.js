4402ea7104b140d7848cf7219e3ff9c185b016cb
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-11 00:29:01 -0400

    Delete src/vfs.js

--- src/vfs.js
+++
@@ -1,3 +0,0 @@
-export const myVFS = {
-  "/node_modules/memfs/index.js": "// src/memfs-entry.js\nimport * as memfs from \"memfs\";\nvar memfs_entry_default = memfs;\nexport {\n  memfs_entry_default as default\n};\n"
-};
