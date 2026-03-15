8f4c172de05a9549b79bda676886d8e1ccbc062d
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-15 01:01:12 -0400

    Update _stream_writeable.js

--- src/_stream_writeable.js
+++ src/_stream_writeable.js
@@ -1,13 +1,21 @@
-import { Writable } from "stream";
+import { Writable } from 'readable-stream';
 
-class WritableState {}
+// In Node, WritableState is an internal class used for managing 
+// the state of the stream. We export it for compatibility.
+const WritableState = Writable.WritableState || class {};
 
-function fromWeb(stream) {
-  return stream;
+/**
+ * Converts a Web WritableStream to a Node Writable stream
+ */
+function fromWeb(webStream, options) {
+  return Writable.fromWeb(webStream, options);
 }
 
-function toWeb(stream) {
-  return stream;
+/**
+ * Converts a Node Writable stream to a Web WritableStream
+ */
+function toWeb(nodeStream) {
+  return Writable.toWeb(nodeStream);
 }
 
 export { Writable, WritableState, fromWeb, toWeb };
