4fa8e9a218abddca69dd93238a667ad0360be5a9
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-16 22:52:15 -0400

    Update globals.js

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,7 @@
-import stdin from 'interals/stdin.js';
+import stdin from './interals/stdin.js';
+import './interals/cli_table.js';
 
 globalThis.process.stdin = stdin;
 
 
-import 'interals/cli_table.js';
+
