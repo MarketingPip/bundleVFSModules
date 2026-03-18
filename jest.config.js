f6cdee277c71a28cdb2d8aa4c586d68eb210803d
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-18 02:06:53 -0400

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  testEnvironment: "node",
+
+  // where tests live
+  testMatch: ["**/*.test.js"],
+
+  // important for modern ESM setups
+  transform: {},
+
+  // optional but useful
+  verbose: true,
+};
