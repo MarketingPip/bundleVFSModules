73c7f164e8a8ab0e443bce0ab3204b4e3209f88c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-18 00:00:30 -0400

    Update globals.js

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,1 @@
-import stdin from './internals/stdin.js';
 import './internals/cli_table.js';
-import makeShim from './internals/stdout.js';
-globalThis.process.stdin = stdin;
-globalThis.process.stdout = makeShim('stdout');
