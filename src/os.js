1052857deb82c0f76b590c9d1a6022f7a29b7a0c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-19 02:40:08 -0400

    Update os.js

--- src/os.js
+++ src/os.js
@@ -166,7 +166,7 @@ export function type() {
  * @returns {string}
  */
 export function release() {
-  return typeof navigator !== 'undefined' ? (navigator.appVersion || '') : '';
+  return typeof navigator !== 'undefined' ? (navigator.appVersion || '') : '5.10.0';
 }
 
 // ---------------------------------------------------------------------------
