1555823165d55a01bfe41a82cbdb31316939856d
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-23 05:47:20 -0400

    Delete src/internals/per_context/primordials.js

--- src/internals/per_context/primordials.js
+++
@@ -1,4 +0,0 @@
-import * as b from "https://github.com/nodejs/node/blob/main/lib/internal/per_context/primordials.js"; // using plugin namespace
-
-export * from "https://github.com/nodejs/node/blob/main/lib/internal/per_context/primordials.js"; // named exports
-export { b as default }; // default export
