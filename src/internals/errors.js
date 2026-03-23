38dd65da2ed0b17cd0a29ff108ad9be3bd92875c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-23 05:20:23 -0400

    Update errors.js

--- src/internals/errors.js
+++ src/internals/errors.js
@@ -1,4 +1,6 @@
+if(!globalThis.primordials){
 import primordials from "./per_context/primordials.js"; // using plugin namespace
+}
 import * as b from "https://github.com/nodejs/node/blob/main/lib/internal/errors.js"; // using plugin namespace
 
 export * from "https://github.com/nodejs/node/blob/main/lib/internal/errors.js"; // named exports
