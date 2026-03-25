5279be853c9d66d1ce76b58b4d8de24854af1d7c
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-25 01:18:07 -0400

    Update node_globals.js

--- src/node_globals.js
+++ src/node_globals.js
@@ -1,4 +1,7 @@
 import stdin from './internals/stdin.js';
 import makeShim from './internals/stdout.js';
+import {patchConsoleTable} from './internals/cli_table.js';
+
+patchConsoleTable(); // turns JSON to proper table
 globalThis.process.stdin = stdin;
 globalThis.process.stdout = makeShim('stdout');
