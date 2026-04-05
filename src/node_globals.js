77e352e2f3df06bd7366ec94984f4b62bd6350fa
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-04-05 11:15:38 -0400

    Import Buffer and timers into globalThis

--- src/node_globals.js
+++ src/node_globals.js
@@ -1,2 +1,8 @@
 import process from './process.js';
+import Buffer from './buffer.js';
+import {clearImmediate, setImmediate} from './timers.js';
 globalThis.process = process;
+globalThis.Buffer = Buffer;
+globalThis.clearImmediate = clearImmediate;
+globalThis.setImmediate = setImmediate;
+ 
