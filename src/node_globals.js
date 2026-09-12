f55e2536f4bd93ea6de303fb14a17e5d2d0ccd1e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-11 20:34:05 -0400

    Add polyfill for queueMicrotask if not defined

--- src/node_globals.js
+++ src/node_globals.js
@@ -6,3 +6,19 @@ globalThis.Buffer = Buffer;
 globalThis.clearImmediate = clearImmediate;
 globalThis.setImmediate = setImmediate;
  
+if (typeof globalThis.queueMicrotask !== 'function') {
+  globalThis.queueMicrotask = function (callback) {
+    if (typeof callback !== 'function') {
+      throw new TypeError('queueMicrotask must be called with a function');
+    }
+
+    Promise.resolve()
+      .then(callback)
+      .catch(error => {
+        // Re-throw errors globally so they aren't silently swallowed
+        setTimeout(() => {
+          throw error;
+        }, 0);
+      });
+  };
+}
