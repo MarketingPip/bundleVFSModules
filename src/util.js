534c6aaf4e488cf7da79d143748eafe4fc6b1003
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-17 01:14:58 -0400

    Add stripVTControlCharacters function to util.js
    Added a function to strip ANSI escape sequences from strings and polyfilled it into the native util module if missing.

--- src/util.js
+++ src/util.js
@@ -6,5 +6,22 @@ import * as util from 'util';
 // Export everything as named exports
 export * from 'util';
 
+// 1. Define the Regex that matches ANSI Escape sequences / VT Control Characters
+const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+
+function stripVTControlCharacters(str) {
+  if (typeof str !== 'string') {
+    throw new TypeError(`The "str" argument must be of type string. Received type ${typeof str}`);
+  }
+  return str.replace(ansiRegex, '');
+}
+
+// 2. Apply it as a polyfill to the native util module if it's missing
+if (util && typeof util.stripVTControlCharacters !== 'function') {
+  util.stripVTControlCharacters = stripVTControlCharacters;
+}
+
+export { stripVTControlCharacters };
+
 // Export the namespace as default
 // export default util;
