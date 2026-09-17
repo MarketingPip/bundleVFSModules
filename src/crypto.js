4402212c9a4ec62619b1c348fa2a62a4df5ba968
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-17 01:51:13 -0400

    Add randomUUID function to global crypto object

--- src/crypto.js
+++ src/crypto.js
@@ -6,10 +6,11 @@ if (typeof globalThis.crypto === 'undefined') {
   globalThis.crypto = {};
 }
 
-if (typeof globalThis.crypto.randomUUID !== 'function') {
-  globalThis.crypto.randomUUID = function randomUUID() {
+
+  
+export function randomUUID() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
       (c ^ (globalThis.crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
     );
   };
-}
+
