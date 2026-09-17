2b02cbfb552e110d168d69a3232ab4125b97ea41
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-17 01:48:46 -0400

    Add polyfill for randomUUID in crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -1,3 +1,15 @@
 import crypto from "crypto-browserify"
 
 export default crypto
+
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = {};
+}
+
+if (typeof globalThis.crypto.randomUUID !== 'function') {
+  globalThis.crypto.randomUUID = function randomUUID() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (c ^ (globalThis.crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    );
+  };
+}
