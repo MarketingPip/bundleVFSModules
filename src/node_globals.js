63173e54c10f9c4865d3d09700378ef2af10117e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-20 00:19:43 -0400

    Update node_globals.js

--- src/node_globals.js
+++ src/node_globals.js
@@ -1,10 +1,6 @@
 import process from './process.js';
 import Buffer from './buffer.js';
 import {clearImmediate, setImmediate} from './timers.js';
-import {patchConsoleTable} from "./internals/cli_table.js";
-
-patchConsoleTable();
-
 globalThis.process = process;
 globalThis.Buffer = Buffer;
 globalThis.clearImmediate = clearImmediate;
