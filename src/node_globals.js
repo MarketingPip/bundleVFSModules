603104292c5ccedce21c19edf69e8d8b40d6c6b3
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-20 01:30:09 -0400

    Fix Buffer assignment in node_globals.js

--- src/node_globals.js
+++ src/node_globals.js
@@ -2,7 +2,7 @@ import process from './process.js';
 import Buffer from './buffer.js';
 import {clearImmediate, setImmediate} from './timers.js';
 //globalThis.process = process;
-globalThis.Buffer = Buffer;
+globalThis.Buffer = Buffer.Buffer;
 globalThis.clearImmediate = clearImmediate;
 globalThis.setImmediate = setImmediate;
  
