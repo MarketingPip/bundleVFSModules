0ec0b54c4c43e4da21a947b5a1db6065d14dfc6a
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-09-20 00:36:11 -0400

    Comment out globalThis.process assignment
    Comment out the assignment of globalThis.process.

--- src/node_globals.js
+++ src/node_globals.js
@@ -1,7 +1,7 @@
 import process from './process.js';
 import Buffer from './buffer.js';
 import {clearImmediate, setImmediate} from './timers.js';
-globalThis.process = process;
+//globalThis.process = process;
 globalThis.Buffer = Buffer;
 globalThis.clearImmediate = clearImmediate;
 globalThis.setImmediate = setImmediate;
